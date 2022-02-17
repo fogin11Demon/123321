d73f4fc681b7a8eef2a5d09de0caea14fe4e4065
Hunter <dekteryo@yandex.ru>
2022-02-17 13:53:30 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("Hello world")),
+          body: Align(alignment: Alignment(0,0), child: Text("Hello world")))));
+}
